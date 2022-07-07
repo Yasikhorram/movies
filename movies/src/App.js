6bbed9c630d5fc5baf87bfef9b16f47e9e664dc1
Yasikhorram <khorramyasaman@gmail.com>
2022-07-07 14:30:43 -0500

    remove strict mode

--- movies/src/App.js
+++ movies/src/App.js
@@ -1,20 +1,10 @@
-import { useEffect } from "react";
-
 const App = () => {
-  const isShown = true;
-  let name = "";
+  let name = prompt("whats your name?");
   console.log("name before is", name);
-
-  useEffect(() => {
-    name = prompt("whats your name?");
-    console.log("name inside is", name);
-  }, []);
-
-  console.log("name after is", name);
 
   return (
     <div className="App">
-      {isShown ? <h1>Hello {name}</h1> : <h3>Hello movies</h3>}
+      {name ? <h1>Hello {name}</h1> : <h3>Hello movies</h3>}
     </div>
   );
 };
