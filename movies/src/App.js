88b2ed7bff7f45bdf5aead778fb55561de5608a4
Yasikhorram <khorramyasaman@gmail.com>
2022-07-06 23:03:34 -0500

    add useeffect

--- movies/src/App.js
+++ movies/src/App.js
@@ -1,5 +1,22 @@
+import { useEffect } from "react";
+
 const App = () => {
-  return <div className="App"></div>;
+  const isShown = true;
+  let name = "";
+  console.log("name before is", name);
+
+  useEffect(() => {
+    name = prompt("whats your name?");
+    console.log("name inside is", name);
+  }, []);
+
+  console.log("name after is", name);
+
+  return (
+    <div className="App">
+      {isShown ? <h1>Hello {name}</h1> : <h3>Hello movies</h3>}
+    </div>
+  );
 };
 
 export default App;
