d7e08d1f665f8d65badcf4ac5294105e1a927f1e
Yasikhorram <khorramyasaman@gmail.com>
2022-07-07 23:25:30 -0500

    add a Person component

--- movies/src/App.js
+++ movies/src/App.js
@@ -1,10 +1,11 @@
+import Person from "./Person";
 const App = () => {
   let name = prompt("whats your name?");
-  console.log("name before is", name);
 
   return (
     <div className="App">
       {name ? <h1>Hello {name}</h1> : <h3>Hello movies</h3>}
+      <Person />
     </div>
   );
 };
---
+++ movies/src/Person.js
@@ -0,0 +1,9 @@
+const Person = () => {
+  return (
+    <div>
+      <h1>Name: Yas</h1>
+    </div>
+  );
+};
+
+export default Person;
